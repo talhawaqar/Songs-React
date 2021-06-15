d57ac6e4de7369dc085e01bdbe6240ca6be305af
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-16 04:37:01 +0500

    combine all reducers and create a store and pass it to provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,22 @@
-export const songsReducer = () => {
+import { combineReducers, conbineReducers } from 'redux';
+
+const songsReducer = () => {
   return [
     { title: 'Titanic Theme', duration: '3:00' },
     { title: 'Edward Maya Present Voilet Lights', duration: '2:00' },
     { title: 'Down Jay Sean', duration: '4:30' },
     { title: 'Show me the meaning of being lonely', duration: '3:10' },
-
   ];
 };
 
-export const selectedSongreducer = (selectedSong = null, action) {
+const selectedSongreducer = (selectedSong = null, action) {
   if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongreducer
+});
