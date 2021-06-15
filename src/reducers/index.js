78053245a16a624d61625e9a033b753458af108a
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-16 04:23:08 +0500

    Add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+export const songsReducer = () => {
+  return [
+    { title: 'Titanic Theme', duration: '3:00' },
+    { title: 'Edward Maya Present Voilet Lights', duration: '2:00' },
+    { title: 'Down Jay Sean', duration: '4:30' },
+    { title: 'Show me the meaning of being lonely', duration: '3:10' },
+
+  ];
+};
+
+export const selectedSongreducer = (selectedSong = null, action) {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
