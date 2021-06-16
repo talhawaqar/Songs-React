5bf3be3e1f2a8ac5435a48a838fc8c339127ca15
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-17 02:27:06 +0500

    Add selected song component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
-    <div className='ui container grid'>
+    <div style={{paddingTop: '50px'}} className='ui container grid'>
       <div className='ui row'>
         <div className='column eight wide'>
           <SongList />
+        </div>
+        <div className='column eight wide'>
+          <SongDetail />
         </div>
       </div>
     </div>
