dec80b92ff700ed30c7b5c65e70cdc3139de97ac
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-16 20:37:42 +0500

    dispatch action selectSong and update state

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
   renderList() {
     return this.props.songs.map((song) => {
-      return(
+      return (
         <div className='item' key={song.title}>
           <div className='right floated content'>
-            <button className='ui button primary'>
+            <button onClick={ ()=> this.props.selectSong(song) }className='ui button primary'>
               Select
             </button>
           </div>
           <div className='content'>
-            { song.title }
+            {song.title}
           </div>
         </div>
       );
@@ -32,4 +33,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
